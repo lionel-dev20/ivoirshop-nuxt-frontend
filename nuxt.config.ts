3543665578a117f3a5e054dc0bb312814d06cae4
lionel Eda <lionel-fabrice.e@glotelhomail.com>
2025-11-22 21:08:15 +0100

    mise ajour google tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -77,6 +77,16 @@ export default defineNuxtConfig({
       script: [
         {
           innerHTML: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K28VRQM');
+          `,
+          type: 'text/javascript'
+        },
+        {
+          innerHTML: `
             !function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
